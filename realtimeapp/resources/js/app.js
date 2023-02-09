c9fd221c27513481a9619584b640f1c351dda0ec
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-09 13:38:26 +0530

    setup public channel configure echo to show notifications to all even if user is not authenticated. To prevent this issue we will use private channel in later commits

--- realtimeapp/resources/js/app.js
+++ realtimeapp/resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+
+Echo.channel("notifications").listen("UserSessionChanged", (ev) => {
+    console.log("HI")
+    const notificationElement = document.getElementById("notification");
+    notificationElement.innerText = ev.message;
+
+    notificationElement.classList.remove("invisible");
+    notificationElement.classList.remove("alert-success");
+    notificationElement.classList.remove("alert-danger");
+
+    notificationElement.classList.add(`alert-${ev.type}`);
+});
