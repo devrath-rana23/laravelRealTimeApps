250c92fce3e1b04bd73856d8d734f28887a76588
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-09 13:11:47 +0530

    make laravel listeners to broadcast and listen to messages

--- realtimeapp/app/Providers/EventServiceProvider.php
+++ realtimeapp/app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Listeners\BroadcastUserLoginNotification;
+use App\Listeners\BroadcastUserLogoutNotification;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -12,12 +16,20 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The event to listener mappings for the application.
      *
+     *  Decides which event to broadcast before and after i.e. sequence in which event is fired from top to down.
+     *
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        Login::class => [
+            BroadcastUserLoginNotification::class,
+        ],
+        Logout::class => [
+            BroadcastUserLogoutNotification::class,
+        ]
     ];
 
     /**
