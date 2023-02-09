10027312d46bb2d9c4e33f53f16d0c9ed5e931b0
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-09 19:51:15 +0530

    make users api and showing users list in web

---
+++ realtimeapp/resources/views/users/showAll.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Users') }}</div>
+
+                    <div class="card-body">
+                        <ul id="users">
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        async function getData(url = '') {
+            const response = await fetch(url, {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+            });
+            return response.json();
+        }
+        getData("/api/users").then((response) => {
+            const usersElement = document.getElementById("users");
+            let users = response;
+            users.forEach((user, index) => {
+                let element = document.createElement("li");
+                element.setAttribute("id", user.id);
+                element.innerText = user.name;
+                usersElement.appendChild(element);
+            });
+        })
+    </script>
+@endsection
